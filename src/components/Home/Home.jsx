2cde95a5f1fcd887a4cd52c56526d0f6b6091b32
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-09 16:10:27 +0600

    tshirt card and grid added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,6 @@
+import './Home.css';
 import { useLoaderData } from "react-router-dom";
+import TShirt from '../TShirt/TShirt';
 
 const Home = () => {
 
@@ -6,8 +8,17 @@ const Home = () => {
     console.log(tshirts);
 
     return (
-        <div>
-            <h3>Home page: {tshirts.length}</h3>
+        <div className="home-layout">
+            <div className="tshirt-area">
+                <div className="tshirt-container">
+                    {
+                        tshirts.map(tshirt => <TShirt key={tshirt._id} tshirt={tshirt}></TShirt>)
+                    }
+                </div>
+            </div>
+            <div className="cart-area">
+                <h3>Cart Area</h3>
+            </div>
         </div>
     );
 };
--- src/components/TShirt/TShirt.jsx
+++ src/components/TShirt/TShirt.jsx
@@ -1,7 +1,16 @@
-const TShirt = () => {
+import './TShirt.css';
+
+const TShirt = ({tshirt}) => {
+
+    const {_id, name, picture, price} = tshirt;
     return (
-        <div>
-            
+        <div className='tshirt-card'>
+            <div className="tshirt-image">
+                <img src={picture} alt="" />
+            </div>
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <button>Buy Now</button>
         </div>
     );
 };
