2381a49a092cdcec35b1d70f13d9c6d431eb1a79
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-10 16:50:40 +0600

    cart style modified

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -20,12 +20,13 @@ const Cart = ({cart, removeFromCart}) => {
 
     return (
         <div>
-            <h3>Quantity: {cart.length} {plural}</h3>
+            <h3>Order Quantity: {cart.length} {plural}</h3>
             <hr />
             <br />
             {massage}
             {
                 cart.map(tshirt => <div className='cart-item' key={tshirt._id}>
+                    <img src={tshirt.picture} alt="" />
                     <p>{tshirt.name}</p>
                     <button onClick={() => removeFromCart(tshirt._id)}>X</button>
                     </div>)
