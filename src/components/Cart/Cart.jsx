5ddfc750bb7ce45d6cc366fccf1855ca770122a7
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-10 22:42:00 +0600

    conditional rendaring modified

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,8 @@ import './Cart.css';
 
 const Cart = ({cart, removeFromCart}) => {
 
-    let massage;
-    if (cart.length === 0) {
-        massage = "Please buy somthing !!";
-    }
+    let message;
+    cart.length === 0 ? message = "Please add somthing !!" : message = "";
 
     let plural;
     if (cart.length === 1) {
@@ -23,13 +21,17 @@ const Cart = ({cart, removeFromCart}) => {
             <h3>Order Quantity: {cart.length} {plural}</h3>
             <hr />
             <br />
-            {massage}
+            {message}
             {
                 cart.map(tshirt => <div className='cart-item' key={tshirt._id}>
                     <img src={tshirt.picture} alt="" />
                     <p>{tshirt.name}</p>
                     <button onClick={() => removeFromCart(tshirt._id)}>X</button>
                     </div>)
+            }
+            <br />
+            {
+                cart.length >= 1 && <p>Thanks for adding !!</p>
             }
         </div>
     );
