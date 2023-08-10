d25cea9bc36a2ce52604eebe41ded857bdf04c52
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-10 13:50:24 +0600

    nav bar style modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <nav className='links'>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/review">Order Review</Link>
-            <Link to="/contact">Contact</Link>
+        <nav className='nav-bar'>
+            <div className='nav-logo'>
+                <h1><a href="/">T-Shirt Hub</a></h1>
+            </div>
+            <div className='nav-links'>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/review">Order Review</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </nav>
     );
 };
